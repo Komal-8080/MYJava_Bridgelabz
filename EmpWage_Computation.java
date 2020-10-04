d993ff94acb1b9d08a7056d66186455eff0c14f7
Komal-8080 <shindeg163@gmail.com>
2020-10-04 20:34:05 +0530

    To do EmpCheck

--- EmpWage_Computation.java
+++ EmpWage_Computation.java
@@ -1,6 +1,13 @@
 
 public class EmpWage_Computation{
 	public static void main(String[] args){
+	int Present = 1; 
+	double empcheck = Math.floor(Math.random()*2);
 		System.out.println("WELCOM TO EMPLOYEE WAGE COMPUTAION PROGRAM");
+		System.out.println(empcheck);
+	if (empcheck == Present)
+		System.out.println("Employee Present");
+	else
+		System.out.println("Employee Absent");
 	}
 }
