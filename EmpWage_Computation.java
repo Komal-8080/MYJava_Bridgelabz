a34f21dbfad84d34b756bbccfa55453ab3e0720e
Komal-8080 <shindeg163@gmail.com>
2020-10-04 23:49:06 +0530

    Fullday Salary added UC2

--- EmpWage_Computation.java
+++ EmpWage_Computation.java
@@ -1,13 +1,19 @@
 
 public class EmpWage_Computation{
 	public static void main(String[] args){
-	int Present = 1; 
-	double empcheck = Math.floor(Math.random()*2);
-		System.out.println("WELCOM TO EMPLOYEE WAGE COMPUTAION PROGRAM");
-		System.out.println(empcheck);
-	if (empcheck == Present)
-		System.out.println("Employee Present");
-	else
-		System.out.println("Employee Absent");
+	//int Present = 1; this is to check for attendence only
+	double empcheck = Math.floor(Math.random()*3);
+			System.out.println("WELCOM TO EMPLOYEE WAGE COMPUTAION PROGRAM");
+			System.out.println(empcheck);
+	int fullTime = 1;
+	int Absent = 0;
+	int wagePerHour = 20;
+        int fullDayHours = 8;
+	int salaryPerDayFT = (20 * 8 );
+		if (empcheck == fullTime)
+				//System.out.println("Employee Present");
+				System.out.println("Salary Per Day for Full Time is : "+salaryPerDayFT);
+		else
+			System.out.println("Employee Absent");
 	}
 }
