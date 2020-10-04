7c59bf5bfa13e3484dd460bec4d9363233749601
Komal-8080 <shindeg163@gmail.com>
2020-10-05 00:58:55 +0530

    Salary Per Month is Added

--- EmpWage_Computation.java
+++ EmpWage_Computation.java
@@ -14,13 +14,16 @@ public class EmpWage_Computation
         int fullDayHours = 8;
 	int salaryPerDayFT = (20 * 8 );
 	int salaryPerDayPT = (20 * 4 );
+	int Month = 20;
 		switch (empcheck) 
 		{
 		case 1:
-			System.out.println("Salary Per Day for Full Time is : "+salaryPerDayFT);
+			System.out.println("Salary Per Day for Full Time is : " +salaryPerDayFT);
+			System.out.println("Salary Per Month for Full Time is : " +(Month*salaryPerDayFT));
 		break;
 		case 2:
-			System.out.println("Salary Per Day for Part Time is : "+salaryPerDayPT);
+			System.out.println("Salary Per Day for Part Time is : " +salaryPerDayPT);
+			System.out.println("Slary per Month for Part Time is : "+(Month*salaryPerDayPT));
 		break;
 		case 0:
 			System.out.println("Employee Absent");
