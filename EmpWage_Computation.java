bfe068e34ab5c17b7f407a63a33c65ba1ecb3f43
Komal-8080 <shindeg163@gmail.com>
2020-10-04 23:55:15 +0530

    Added Part TIme

--- EmpWage_Computation.java
+++ EmpWage_Computation.java
@@ -6,13 +6,17 @@ public class EmpWage_Computation{
 			System.out.println("WELCOM TO EMPLOYEE WAGE COMPUTAION PROGRAM");
 			System.out.println(empcheck);
 	int fullTime = 1;
+	int partTime = 2;
 	int Absent = 0;
 	int wagePerHour = 20;
         int fullDayHours = 8;
 	int salaryPerDayFT = (20 * 8 );
+	int salaryPerDayPT = (20 * 4 );
 		if (empcheck == fullTime)
 				//System.out.println("Employee Present");
-				System.out.println("Salary Per Day for Full Time is : "+salaryPerDayFT);
+			System.out.println("Salary Per Day for Full Time is : "+salaryPerDayFT);
+		else if (empcheck == partTime)
+			System.out.println("Salary Per Day for Part Time is : "+salaryPerDayPT);
 		else
 			System.out.println("Employee Absent");
 	}
