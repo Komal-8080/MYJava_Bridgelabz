99c1eacdf538b23416988c7e3b8625855772f4b5
Komal-8080 <shindeg163@gmail.com>
2020-10-06 15:04:48 +0530

    UC2

--- EmpWagePractice.java
+++ EmpWagePractice.java
@@ -7,12 +7,19 @@ public class EmpWagePractice
 		System.out.println("Attendence status : " +attendence);
 	int full_time = 1;
 	int Absent = 0;
+	int empWagePerHour = 20;
+	int empHrs = 0;
+	int empWage = 0;
 
 	if (attendence == full_time) {
 		System.out.println("Employee Present");
+	empHrs = 8;
 	}
 	else {
 		System.out.println("Employee Absent");
+	empHrs = 0;
 	}
+	empWage = empHrs * empWagePerHour;
+		System.out.println("Employee Salary Per Day is : " + empWage);
 	}
 }
