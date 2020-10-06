1eda0a882a3c3a133dc0bfde92b712aefa1e6714
Komal-8080 <shindeg163@gmail.com>
2020-10-06 15:31:34 +0530

    UC5

--- EmpWagePractice.java
+++ EmpWagePractice.java
@@ -3,16 +3,20 @@ public class EmpWagePractice
 	public static void main(String[] args)
 	{
 		System.out.println(" !! WELCOME TO EMPLOYEE WAGE COMPUTATION !!");
-	int attendence = (int)(Math.random() * 3);
-		System.out.println("Attendence status : " +attendence);
+
 	final int full_time = 1;
 	final int part_time = 2;
 	final int Absent = 0;
 	final int empWagePerHour = 20;
+	final int empWorkingDaysInMonth = 20;
 	int empHrs = 0;
 	int empWage = 0;
-
-switch (attendence){
+	int wagePerMonth = 0;
+	
+	for (int day=0; day<empWorkingDaysInMonth; day++){
+	 int attendence = (int)(Math.random() * 3);
+                System.out.println("Attendence status : " +attendence);
+	switch (attendence){
 	case full_time:
 		System.out.println("Employee Present");
 		empHrs = 8;
@@ -26,6 +30,9 @@ switch (attendence){
 		empHrs = 0;
 	}
 	empWage = empHrs * empWagePerHour;
+	wagePerMonth += empWage;
 		System.out.println("Employee Salary Per Day is : " + empWage);
+	}
+		System.out.println("Total Wages Per Month: " +wagePerMonth);
 	}
 }
