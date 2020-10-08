79e411b2da88395132c2f8882079de52147532db
Komal-8080 <shindeg163@gmail.com>
2020-10-08 21:03:07 +0530

    UC7

--- EmpWagePractice.java
+++ EmpWagePractice.java
@@ -1,15 +1,14 @@
 public class EmpWagePractice 
 {
-	public static void main(String[] args)
-	{
-		System.out.println(" !! WELCOME TO EMPLOYEE WAGE COMPUTATION !!");
+	public static final int full_time = 1;
+	public static final int part_time = 2;
+	public static final int Absent = 0;
+	public static final int empWagePerHour = 20;
+	public static final int empWorkingDaysInMonth = 20;
+	public static final int MaxHours = 100;
 
-	final int full_time = 1;
-	final int part_time = 2;
-	final int Absent = 0;
-	final int empWagePerHour = 20;
-	final int empWorkingDaysInMonth = 20;
-	final int MaxHours = 100;
+	public static int computeEmpWage()
+	{
 	int empHrs = 0;
 	int totalHours = 0;
 	int totalWorkingDays = 0;
@@ -38,5 +37,12 @@ public class EmpWagePractice
 	}
 	int wagePerMonth = totalHours * empWagePerHour;
 		System.out.println("Total Wages Per Month: " +wagePerMonth);
+	return wagePerMonth;
 	}
+	 public static void main(String[] args)
+        {
+                System.out.println(" !! WELCOME TO EMPLOYEE WAGE COMPUTATION !!");
+		computeEmpWage();
+	}
+
 }
